2e0f293ceee395e772f41d37c8c284a0702e932d
romangordeew <romangordeew@gmail.com>
2025-07-21 11:09:56 +0200

    tw widget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
-import TradeForm from '@/components/trade-form/trade-form'
-import VibeTrader from '@/components/vibe-trader/vibe-trader'
+import TradingViewWidget from "@/components/chart/chart";
+import TradeForm from "@/components/trade-form/trade-form";
+import VibeTrader from "@/components/vibe-trader/vibe-trader";
 
 export default function Home() {
-  return (
-    <div className="flex flex-1 justify-center items-center">
-      <div className="flex flex-row gap-4">
-        <TradeForm />
-        <VibeTrader />
-      </div>
-    </div>
-  )
+	return (
+		<div className="flex flex-1 flex-col justify-center items-center">
+			<div className="mb-4">
+				<TradingViewWidget />
+			</div>
+			<div className="flex flex-row gap-4">
+				<TradeForm />
+				<VibeTrader />
+			</div>
+		</div>
+	);
 }
---
+++ src/components/chart/chart.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { memo, useEffect, useRef } from 'react'
+
+function TradingViewWidget() {
+  const container = useRef(null)
+
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src =
+      'https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js'
+    script.type = 'text/javascript'
+    script.async = true
+    script.innerHTML = `
+        {
+          "allow_symbol_change": true,
+          "calendar": false,
+          "details": false,
+          "hide_side_toolbar": true,
+          "hide_top_toolbar": false,
+          "hide_legend": false,
+          "hide_volume": false,
+          "hotlist": false,
+          "interval": "1",
+          "locale": "en",
+          "save_image": true,
+          "style": "1",
+          "symbol": "BINANCE:DOGEUSDT",
+          "theme": "dark",
+          "timezone": "Etc/UTC",
+          "backgroundColor": "#0F0F0F",
+          "gridColor": "rgba(242, 242, 242, 0.06)",
+          "watchlist": [],
+          "withdateranges": false,
+          "compareSymbols": [],
+          "studies": [],
+          "width": 890,
+          "height": 500
+        }`
+    // @ts-ignore
+    container.current.appendChild(script)
+  }, [])
+
+  return (
+    <div className="tradingview-widget-container" ref={container}>
+      <div className="tradingview-widget-container__widget"></div>
+    </div>
+  )
+}
+
+export default memo(TradingViewWidget)
